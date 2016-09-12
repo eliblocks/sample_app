bc92f6de0042d645bbc0fe166403305f17b14cb5
Eli Block <eliblock@yahoo.com>
2016-09-12 17:44:58 +0000

    create ccsf controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'ccsf_rails/index'
+
+  get 'ccsf_rails/links'
+
+  get 'ccsf_rails/about'
+  
+  get 'ccsf_rails/assignments'
+
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   root 'application#hello'
 end
